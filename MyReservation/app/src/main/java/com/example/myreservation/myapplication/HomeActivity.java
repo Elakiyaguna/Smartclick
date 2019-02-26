82893aa139e9aaef59e3611f7e6c76df8ef3de86
elakiyaguna <gunaelakiya@gmail.com>
2019-02-26 21:52:33 +0530

    Latest code
    Latest code

---
+++ MyReservation/app/src/main/java/com/example/myreservation/myapplication/HomeActivity.java
@@ -0,0 +1,59 @@
+package com.example.myreservation.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RelativeLayout parkingRL = findViewById(R.id.layout_parking);
+        RelativeLayout diningRL = findViewById(R.id.layout_dining);
+        RelativeLayout commuteRL = findViewById(R.id.layout_commute);
+
+        parkingRL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, ParkingMainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        diningRL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        commuteRL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+}
