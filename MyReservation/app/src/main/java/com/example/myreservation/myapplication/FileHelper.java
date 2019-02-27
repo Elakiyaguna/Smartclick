d77abf1948ca5d0c135a66bda00dc1ae38e13a93
elakiyaguna <gunaelakiya@gmail.com>
2019-02-27 19:50:25 +0530

    Addddddding file concept
    Addddddding file concept

---
+++ MyReservation/app/src/main/java/com/example/myreservation/myapplication/FileHelper.java
@@ -0,0 +1,39 @@
+package com.example.myreservation.myapplication;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileHelper {
+    final static String fileName = "data.txt";
+    final static String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/smartclick/readwrite/" ;
+    final static String TAG = FileHelper.class.getName();
+    public static boolean saveToFile( String data){
+        try {
+            new File(path  ).mkdir();
+            File file = new File(path+ fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream(file,true);
+            fileOutputStream.write((data + System.getProperty("line.separator")).getBytes());
+
+            return true;
+        }  catch(FileNotFoundException ex) {
+            Log.d(TAG, ex.getMessage());
+        }  catch(IOException ex) {
+            Log.d(TAG, ex.getMessage());
+        }
+        return  false;
+
+
+    }
+}
